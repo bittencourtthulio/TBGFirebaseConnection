46f6a358f1bfe488d865ff05b4ff8a4180c8d118
Anderson Ramos <anderson.ramlug@gmail.com>
2021-03-25 13:45:47 -0300

    remove warnings and hints

---
+++ TBGFirebaseConnection.Interfaces.pas
@@ -0,0 +1,125 @@
+unit TBGFirebaseConnection.Interfaces;
+
+interface
+
+uses System.JSON, Data.DB, System.Classes;
+
+//teste
+
+type
+  iFirebaseConnect = interface;
+  iFirebasePut = interface;
+  iFirebaseGet = interface;
+  iFirebasePatch = interface;
+  iFirebaseDelete = interface;
+  iFirebaseCloudMessage = interface;
+  iFirebaseCloudMessageRegistration = interface;
+  iFirebasePost = interface;
+
+  iFirebaseConnection = interface
+    ['{D24B1AB1-2AA9-46E3-8DB8-5CB550A2BD17}']
+    function Connect : iFirebaseConnect;
+    function Put : iFirebasePut;
+    function Get : iFirebaseGet;
+    function Patch : iFirebasePatch;
+    function Post : iFirebasePost;
+    function Delete : iFirebaseDelete;
+    function CloudMessage : iFirebaseCloudMessage;
+    procedure &Exec;
+  end;
+
+  iFirebaseConnect = interface
+    ['{CAABD8E2-3530-4D50-9EBB-225B45E6A686}']
+    function BaseURL (Value : String) : iFirebaseConnect; overload;
+    function Auth (Value : String) : iFirebaseConnect; overload;
+    function uId (Value : String) : iFirebaseConnect; overload;
+    function BaseURL : String; overload;
+    function Auth : String; overload;
+    function uId : String; overload;
+    function &End : iFirebaseConnection;
+  end;
+
+  iFirebasePut = interface
+    ['{15826039-2077-4FA3-A952-5F6EE716664E}']
+    function Resource ( Value : String) : iFirebasePut; overload;
+    function Resource : String; overload;
+    function Json ( Value : String) : iFirebasePut; overload;
+    function Json ( Value : TJsonObject) : iFirebasePut; overload;
+    function Json ( Value : TJsonArray ) : iFirebasePut; overload;
+    function Json : String; overload;
+    function &End : iFirebaseConnection;
+  end;
+
+  iFirebasePatch = interface
+    ['{15826039-2077-4FA3-A952-5F6EE716664E}']
+    function RegisterID ( aValue : Integer ) : iFirebasePatch; overload;
+    function Resource ( Value : String) : iFirebasePatch; overload;
+    function Resource : String; overload;
+    function Json ( Value : String) : iFirebasePatch; overload;
+    function Json ( Value : TJsonObject) : iFirebasePatch; overload;
+    function Json ( Value : TJsonArray ) : iFirebasePatch; overload;
+    function Json : String; overload;
+    function &End : iFirebaseConnection;
+  end;
+
+  iFirebasePost = interface
+    ['{15826039-2077-4FA3-A952-5F6EE716664E}']
+    function Resource ( Value : String) : iFirebasePost; overload;
+    function Resource : String; overload;
+    function Json ( Value : String) : iFirebasePost; overload;
+    function Json ( Value : TJsonObject) : iFirebasePost; overload;
+    function Json ( Value : TJsonArray ) : iFirebasePost; overload;
+    function Json : String; overload;
+    function &End : iFirebaseConnection;
+  end;
+
+  iFirebaseDelete = interface
+    ['{15826039-2077-4FA3-A952-5F6EE716664E}']
+    function RegisterID ( aValue : Integer ) : iFirebaseDelete; overload;
+    function Resource ( Value : String) : iFirebaseDelete; overload;
+    function Resource : String; overload;
+    function Json ( Value : String) : iFirebaseDelete; overload;
+    function Json ( Value : TJsonObject) : iFirebaseDelete; overload;
+    function Json : String; overload;
+    function &End : iFirebaseConnection;
+  end;
+
+  iFirebaseGet = interface
+    ['{42656C46-ADD4-4F7D-AA15-DB4E9437CF55}']
+    function DataSet ( Value : TDataSet ) : iFirebaseGet; overload;
+    function DataSet : TDataSet; overload;
+    function ResponseContent(var aResponse : String ) : iFirebaseGet; overload;
+    function ResponseContent(var aJsonArray : TJsonArray) : iFirebaseGet; overload;
+    function ResponseContent(var aJsonObject : TJsonObject) : iFirebaseGet; overload;
+    function Resource (Value : String) : iFirebaseGet; overload;
+    function Resource : String; overload;
+    function &End : iFirebaseConnection;
+  end;
+
+  iFirebaseCloudMessage = interface
+    ['{4B4EBB38-10D2-4CAA-812C-3A0A900DFFA5}']
+    function BaseURL( aBaseURL : String ) : iFirebaseCloudMessage;
+    function Auth ( aAuth : String ) : iFirebaseCloudMessage;
+    function UID ( aUID : String ) : iFirebaseCloudMessage;
+    function RemetenteCode(aCode : String) : iFirebaseCloudMessage;
+    function APIKey(aApi : String) : iFirebaseCloudMessage;
+    function AddDeviceAPI(aDeviceToken : String) : iFirebaseCloudMessage;
+    function AddDeviceList(aDeviceList : TStringList) : iFirebaseCloudMessage;
+    function SendMessage(aMessage : String) : iFirebaseCloudMessage;
+    function Send : iFirebaseCloudMessage;
+    function RegisterPush : iFirebaseCloudMessageRegistration;
+  end;
+
+  iFirebaseCloudMessageRegistration = interface
+    ['{CCBA59F7-2B91-4F92-8323-4C7610B6C873}']
+    function RegisterPushService : iFirebaseCloudMessageRegistration;
+    function BaseURL(aURL : String) : iFirebaseCloudMessageRegistration;
+    function Auth(aAuth : String) : iFirebaseCloudMessageRegistration;
+    function uID(aUID : String) : iFirebaseCloudMessageRegistration;
+    function Resource(aResource : String) : iFirebaseCloudMessageRegistration;
+    function RemetenteCode(aCode : String) : iFirebaseCloudMessageRegistration;
+  end;
+
+implementation
+
+end.
---
+++ TBGFirebaseConnection.Model.Get.pas
@@ -0,0 +1,158 @@
+unit TBGFirebaseConnection.Model.Get;
+
+interface
+
+uses
+  TBGFirebaseConnection.Interfaces,
+  Data.DB,
+  Data.DBXJSONReflect,
+  REST.Client,
+  REST.Types,
+  REST.JSON,
+  REST.Response.Adapter,
+  IpPeerClient,
+  System.JSON;
+
+Type
+  TFirebaseConnectionModelGet = class(TInterfacedObject, iFirebaseGet)
+  private
+    FParent : iFirebaseConnection;
+    FDataSet : TDataSet;
+    FResource : String;
+    FRESTRequest : TRESTRequest;
+    FRESTResponse : TRESTResponse;
+    FRESTClient : TRESTClient;
+    FRESTResponseDataSetAdapter : TRESTResponseDataSetAdapter;
+    FResponseJson: string;
+    FResponseJsonArray : ^TJSONArray;
+    FResponseJsonObject : ^TJSONObject;
+  public
+    constructor Create(Parent : iFirebaseConnection);
+    destructor Destroy; override;
+    class function New(Parent : iFirebaseConnection) : iFirebaseGet;
+    function DataSet(Value: TDataSet): iFirebaseGet; overload;
+    function DataSet: TDataSet; overload;
+    function Resource(Value: String): iFirebaseGet; overload;
+    function Resource: String; overload;
+    function ResponseContent(var aResponse : String ) : iFirebaseGet; overload;
+    function ResponseContent(var aJsonArray : TJsonArray) : iFirebaseGet; overload;
+    function ResponseContent(var aJsonObject : TJsonObject) : iFirebaseGet; overload;
+    function &End: iFirebaseConnection;
+  end;
+
+implementation
+
+uses
+  System.SysUtils;
+
+{ TFirebaseConnectionModelGet_ }
+
+function TFirebaseConnectionModelGet.&End: iFirebaseConnection;
+begin
+  Result := FParent;
+  try
+    FRESTResponseDataSetAdapter.Dataset := FDataSet;
+    FRESTClient.BaseURL := FParent.Connect.BaseURL;
+    FRESTRequest.Method := rmGET;
+    FRESTRequest.resource := FResource;
+    FRESTRequest.Execute;
+    FResponseJson := FRESTResponse.Content;
+
+    try
+      if FResponseJson <> 'null' then
+        if Copy(FResponseJson, 0, 1) = '[' then
+          FResponseJsonArray^ := TJSONObject.ParseJSONValue(TEncoding.ASCII.GetBytes(FResponseJson),0) as TJSONArray
+        else
+          FResponseJsonObject^ := TJSONObject.ParseJSONValue(FResponseJson) as TJSONObject;
+    except
+      //not result
+    end;
+
+
+    if Assigned(FDataSet) then
+    begin
+      FRESTResponseDataSetAdapter.Active := true;
+      FDataSet.Active := True;
+    end;
+
+  except
+    raise Exception.Create('Erro ao Consultar a Base de Dados');
+  end;
+end;
+
+constructor TFirebaseConnectionModelGet.Create(Parent : iFirebaseConnection);
+begin
+  FResponseJsonArray := nil;
+  FParent := Parent;
+  FRESTRequest := TRESTRequest.Create(nil);
+  FRESTResponse := TRESTResponse.Create(FRESTRequest);
+  FRESTClient := TRESTClient.Create(FParent.Connect.BaseURL);
+  FRESTClient.Accept := 'application/json, text/plain; q=0.9, text/html;q=0.8,';
+  FRESTClient.AcceptCharset := 'UTF-8, *;q=0.8';
+  FRESTClient.AcceptEncoding := 'identity';
+  FRESTRequest.Accept := 'application/json, text/plain; q=0.9, text/html;q=0.8,';
+  FRESTRequest.AcceptCharset := 'UTF-8, *;q=0.8';
+  FRESTRequest.AcceptEncoding := 'identity';
+  FRESTRequest.Client := FRESTClient;
+  FRESTRequest.Response := FRESTResponse;
+  //FMemTable := TFDMemTable.Create(FRESTRequest);
+  FRESTResponseDataSetAdapter := TRESTResponseDataSetAdapter.Create(FRESTRequest);
+  //FRESTResponseDataSetAdapter.Dataset := FMemTable;
+  FRESTResponseDataSetAdapter.Response := FRESTResponse;
+end;
+
+function TFirebaseConnectionModelGet.DataSet(Value: TDataSet): iFirebaseGet;
+begin
+  Result := Self;
+  FDataSet := Value;
+end;
+
+function TFirebaseConnectionModelGet.DataSet: TDataSet;
+begin
+  Result := FDataSet;
+end;
+
+destructor TFirebaseConnectionModelGet.Destroy;
+begin
+
+  inherited;
+end;
+
+class function TFirebaseConnectionModelGet.New(Parent : iFirebaseConnection) : iFirebaseGet;
+begin
+  Result := Self.Create(Parent);
+end;
+
+function TFirebaseConnectionModelGet.Resource(Value: String): iFirebaseGet;
+begin
+  Result := Self;
+  FResource := '/' + Value + '.json';
+end;
+
+function TFirebaseConnectionModelGet.Resource: String;
+begin
+  Result := FResource;
+end;
+
+function TFirebaseConnectionModelGet.ResponseContent(
+  var aJsonObject: TJsonObject): iFirebaseGet;
+begin
+  Result := Self;
+  FResponseJsonObject := @aJsonObject;
+end;
+
+function TFirebaseConnectionModelGet.ResponseContent(var
+  aJsonArray: TJsonArray): iFirebaseGet;
+begin
+  Result := Self;
+  FResponseJsonArray := @aJsonArray;
+end;
+
+function TFirebaseConnectionModelGet.ResponseContent(var
+  aResponse: String): iFirebaseGet;
+begin
+  Result := Self;
+  FResponseJson := aResponse;
+end;
+
+end.
